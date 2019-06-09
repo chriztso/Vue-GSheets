46a7d10574ee5c59e7a9608b494b533c2bbe3da8
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-09 14:40:20 -0500

    Added get route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var addRow = require('./spreadsheet.js').addRow;
+var getRows = require('./spreadsheet.js').getRows;
 
 var port = 3000; 
 
@@ -9,6 +10,15 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json())
 
+app.get('/users', (req, res) => {
+    getRows((err, data) => {
+        if(err){
+            res.status(404).send(err);
+            return;
+        }
+        res.status(200).send(data);
+    })
+})
 app.post('/users', (req, res) => {
     addRow(req.body.first, req.body.last, req.body.phone, (err, data) => {
         if(err){
