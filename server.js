a8ee508158d5ea37741a650e690b89f4a5d2a7fc
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-09 13:53:28 -0500

    Created post route

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var addRow = require('./spreadsheet.js').addRow;
 
 var port = 3000; 
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json())
+
+app.post('/users', (req, res) => {
+    addRow(req.body.first, req.body.last, req.body.phone, (err, data) => {
+        if(err){
+            res.status(404).send(err);
+            return;
+        }
+        res.status(200).send(data);
+    })
+    
+})
 
 app.listen(port, () => {console.log('listening at 3000')});
