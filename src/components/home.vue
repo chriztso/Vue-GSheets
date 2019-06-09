f93a5bc8f1b954e80d15aba13e2790ea07244a92
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-09 13:05:15 -0500

    Created form with add user method

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,1 +1,37 @@
+<template>
+  <div>
+    First Name: <input type = 'text' v-model = "first">
+    Last Name: <input type = 'text' v-model = "last">
+    Phone Number: <input type = 'text' v-model = "phone">
+    <input type = 'submit' value = 'Enter info'  v-on:click = "addInfo">
+  </div>
+</template>
 
+<script>
+import axios from 'axios';
+
+  export default{
+    data(){
+      return {
+        first : '', 
+        last : '', 
+        phone : ''
+      }
+    },
+    methods : {
+        addInfo(){
+            axios.post('/users', {
+                first : this.first, 
+                last : this.last, 
+                phone : this.phone
+            })
+            .then(() => {
+                console.log('added');
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
+    }
+  }
+</script>  
