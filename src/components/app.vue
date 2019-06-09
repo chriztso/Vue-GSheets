e8e04aba46bbd9d3eace0c0a846f192d7b85d219
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-09 13:05:40 -0500

    Created app file

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,1 +1,16 @@
+<template> 
+  <div>
+    Enter your information:
+    <Home />
+  </div>
+</template>
 
+<script>
+  import Home from './home.vue';
+
+  export default{
+    components :{
+        Home
+    }
+  }
+</script>
